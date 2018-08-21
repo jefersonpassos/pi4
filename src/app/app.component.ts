705a41a8e49a1cacf3ba8804b3046fbc5b6d786f
rodrigo <digorinaldi@gmail.com>
2018-08-20 22:06:10 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+declare var UIkit: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pi4';
+
+  showAlert(): void {
+    UIkit.modal.alert('UIkit alert!');
+  }
+}
